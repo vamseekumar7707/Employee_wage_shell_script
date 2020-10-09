932bcd6996554f210e2638a15b766d6598459d7a
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-09 12:41:12 +0530

    Added: Store_Day_Daily_Total_wages

--- employee.sh
+++ employee.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HOUR=20
@@ -7,6 +8,8 @@ MAX_HPURS_IN_MONTHS=40
 
 totalEmpHours=0
 totalWorkingDays=0
+
+declare -A dailyWages
 
 function getWorkingHours(){
         case $1 in
@@ -23,11 +26,20 @@ function getWorkingHours(){
         echo $empHrs
 }
 
+function getEmpWage(){
+        oneDayHrs=$1
+        echo $(($oneDayHrs*$EMP_RATE_PER_HOUR))
+}
 while [[ $totalEmpHours -lt $MAX_HPURS_IN_MONTHS && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
 do
         ((totalWorkingDays++))
         empHours=$( getWorkingHours $((RANDOM%3)) )
         totalEmpHours=$(($totalEmpHours+$empHours))
+        dailyWages[$totalWorkingDays]=$( getEmpWage $empHours )
 done
         totalSalary=$(($EMP_RATE_PER_HOUR*$totalEmpHours))
         echo "total salary is : $totalSalary"
+        echo "Daily wages : ${dailyWages[@]}"
+        echo "Daily index : ${!dailyWages[@]}"
+
+
